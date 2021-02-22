e3bed75df79bf540a95f4fa8026216b18220d120
HarshaRajeev888 <harsharajeev777@gmail.com>
2021-02-22 14:16:37 +0530

    test

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -12,5 +12,39 @@ export class TodoComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  todo:any[]=[]
+  to:any[]=[]
+  task:string=''
+  s:any
+  
+  addtask(event:any){
+    this.task=(event.target as HTMLInputElement).value
+    }
+  add(){
+    this.todo.push(this.task)
+    console.log( this.todo);
+  }
+  removearr(a:any){
+    this.s=this.todo.indexOf(a)
+    this.todo.splice(this.s,1)
+    console.log(this.todo)
+  }
+  movearr( a:any){
+    this.to.push(a)
+    this.removearr(a)
+  }
+  del(a:any){
+    this.s=this.to.indexOf(a)
+    this.to.splice(this.s,1)
+    console.log(this.to)
+  
+  }
+  clear()
+  {
+    this.task=""
+      
+    }
 
+  
 }
+
